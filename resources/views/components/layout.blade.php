5ad320b1b869b89e7c6cb8f21aca985151d6a634
Fabio Trani <trani.fabio.90@gmail.com>
2022-01-22 00:42:30 +0100

    easter egg

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,14 +2,6 @@
 <html lang="it">
 
 <head>
-  
-███████╗  ████████╗
-██╔════╝  ╚══██╔══╝
-█████╗░░  ░░░██║░░░
-██╔══╝░░  ░░░██║░░░
-██║░░░░░  ░░░██║░░░
-╚═╝░░░░░  ░░░╚═╝░░░
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -35,6 +27,14 @@
 </head>
 
 <body>
+    <div style="display: none">
+        ███████╗  ████████╗
+        ██╔════╝  ╚══██╔══╝
+        █████╗░░  ░░░██║░░░
+        ██╔══╝░░  ░░░██║░░░
+        ██║░░░░░  ░░░██║░░░
+        ╚═╝░░░░░  ░░░╚═╝░░░
+    </div>
     <x-navbar />
     <div itemscope itemtype="https://www.petershouse.it/Person">
 
